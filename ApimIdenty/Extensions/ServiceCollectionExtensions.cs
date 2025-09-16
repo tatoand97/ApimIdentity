f16897d7446d496f0f1b1cd71fe6d21a79afc6d6
Ricardo A Leyva <25676407+tatoand97@users.noreply.github.com>
2025-09-16 12:27:35 -0500

    Update ServiceCollectionExtensions.cs

--- ApimIdenty/Extensions/ServiceCollectionExtensions.cs
+++ ApimIdenty/Extensions/ServiceCollectionExtensions.cs
@@ -31,4 +31,47 @@ public static class ServiceCollectionExtensions
 
         return services;
     }
+
+    public static IServiceCollection AddHttpClientConfiguration(this IServiceCollection services)
+    {
+        var serviceConfiguration = GetServiceConfiguration(services);
+
+        services.AddHttpClient();
+        services.AddHttpClientWrapper(options =>
+        {
+            Dictionary<string, RestClientServiceSetting> restClientServices = [];
+            options.Services = restClientServices;
+
+            options.HttpClientRetry = serviceConfiguration.HttpClientRetry;
+            options.HttpClientDelay = serviceConfiguration.HttpClientDelay;
+
+            TokenSetting setting = new()
+            {
+                Path = serviceConfiguration.TokenSetting.Path,
+                GrantType = serviceConfiguration.TokenSetting.GrantType,
+                ClientId = serviceConfiguration.TokenSetting.ClientId,
+                Scope = serviceConfiguration.TokenSetting.Scope,
+                ClientSecret = serviceConfiguration.TokenSetting.ClientSecret,
+                ContentType = serviceConfiguration.TokenSetting.ContentType,
+                DefaultRequestHeaders = new Dictionary<string, string>(serviceConfiguration.TokenSetting.DefaultRequestHeaders),
+                BaseAddress = serviceConfiguration.TokenSetting.BaseAddress
+            };
+
+            options.Services.Add(Constants.TUYASERVICEKEYINGRESS, new RestClientServiceSetting()
+            {
+                BaseAddress = (serviceConfiguration.TuyaIngressService),
+            });
+
+            options.Services.Add(Constants.TUYASERVICEKEY, new RestClientServiceSetting()
+            {
+                BaseAddress = (serviceConfiguration.TuyaService),
+                DefaultRequestHeaders = setting.DefaultRequestHeaders,
+                TokenRequest = setting
+            });
+        });
+
+        //services.AddTransient<IClienteinformacionServices, ClienteinformacionServices>();
+
+        return services;
+    }
 }
